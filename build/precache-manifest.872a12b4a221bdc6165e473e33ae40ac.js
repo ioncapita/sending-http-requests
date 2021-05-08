70fa11aa78e4920246096449bee3f0ed8fb276ff
ioncapita <yonelcapita@gmail.com>
2021-05-08 23:56:44 +0100

    first commit

---
+++ build/precache-manifest.872a12b4a221bdc6165e473e33ae40ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d15575633cd687d65c479597121d633a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c70e2b4095b24598ce3f",
+    "url": "/static/css/main.e18dbe99.chunk.css"
+  },
+  {
+    "revision": "a8b9dea63737512022ad",
+    "url": "/static/js/2.03b8e3cf.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.03b8e3cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c70e2b4095b24598ce3f",
+    "url": "/static/js/main.295e6013.chunk.js"
+  },
+  {
+    "revision": "f13d5fb20258b992088c",
+    "url": "/static/js/runtime-main.a6b2f021.js"
+  }
+]);
